b4e4ee774234dbba49a287c1e03d1c704331a73b
Mayur Halai <halai_mayur@yahoo.com>
2026-06-10 18:08:37 +0100

    Track todos

--- scripts/kind-install.sh
+++ scripts/kind-install.sh
@@ -10,6 +10,9 @@ kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/downl
 # To install the extensions components:
 kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/extensions.yaml
 
+# Create namespace
+kubectl create namespace cloud-agent
+
 # Install sandbox router
 AGENT_SANDBOX_REPO_LOCATION="../../kubernetes-sigs/agent-sandbox"
 pushd "${AGENT_SANDBOX_REPO_LOCATION}/clients/python/agentic-sandbox-client/sandbox-router"
@@ -17,11 +20,8 @@ SANDBOX_ROUTER_IMAGE="sandbox-router:latest"
 docker build -t $SANDBOX_ROUTER_IMAGE .
 kind load docker-image $SANDBOX_ROUTER_IMAGE --name desktop
 # sed -i "s|\${ROUTER_IMAGE}|${SANDBOX_ROUTER_IMAGE}|g" sandbox_router.yaml
-kubectl apply -n agent-sandbox-system -f sandbox_router.yaml
+kubectl apply -n cloud-agent -f sandbox_router.yaml
 popd
-
-# Create namespace
-kubectl create namespace cloud-agent
 
 # Create secret
 kubectl apply -f /Users/mayurhalai/repos/crap/secrets/cloud-agent-secret.yaml -n cloud-agent
